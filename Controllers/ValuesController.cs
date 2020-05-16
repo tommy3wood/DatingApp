f93c9bbc6a610aaa710216b7f6606479abc8bea5
tommy3wood <thomaswcurran1014@gmail.com>
2020-05-16 14:29:46 -0500

    value model/datacontext

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -11,13 +11,15 @@ namespace DatingApp.API.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        // index 
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value3" };
         }
 
         // GET api/values/5
+        // show 
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,18 +27,21 @@ namespace DatingApp.API.Controllers
         }
 
         // POST api/values
+        // create  
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
+        // update 
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
+        // destroy
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
